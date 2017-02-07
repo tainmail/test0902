9f704a6c870e9129f625e70d337c8073cdb00839
tainmail <tainmail@gmail.com>
2017-02-07 16:36:55 +0800

    cc
    ccc

--- [邏輯 原理]xxxxxxx/000-田開發模組/jquery-旋轉木馬適應整頁滾動/原始檔案/新增資料夾/oeya2.js
+++
@@ -1,8 +0,0 @@
-﻿var manu_code = 'p3TUha6viG%2FJ84Q';
-var code='member';
-var code_value='$member';
-var code_sub='1';
-var pro_info='';
-var action_name = '';
-url = pkBaseURL + "track/oeya_url_cookie.php";
-oeya_log(action_name, manu_code, url,code,code_value,code_sub,pro_info);
